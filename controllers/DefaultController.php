33efed9e4437f22e444cd2ecdb496405fcfcfa06
dnuessli <daniel.nuessli@ostendis.com>
2021-02-10 13:22:29 +0100

    Bugfix gzip

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -11,12 +11,15 @@ class DefaultController extends \yii\base\Controller
         $module = $this->module;
         if (!$sitemapData = $module->cacheProvider->get($module->cacheKey)) {
             $sitemapData = $module->buildSitemap();
-            $sitemapData = gzencode($sitemapData);
             $module->cacheProvider->set($module->cacheKey, $sitemapData, $module->cacheExpire);
         }
         Yii::$app->response->format = \yii\web\Response::FORMAT_RAW;
         $headers = Yii::$app->response->headers;
         $headers->add('Content-Type', 'application/xml');
+        $sitemapData = gzencode($sitemapData);
+        $headers->add('Content-Encoding', 'gzip');
+        $headers->add('Content-Length', strlen($sitemapData));
+        
         return $sitemapData;
     }
 }
